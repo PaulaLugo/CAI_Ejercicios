6b7321e6c748615cf492642ed782e4110800ba6d
PaulaLugo <48LU17619093@campus.economicas.uba.ar>
2020-09-03 23:44:12 -0300

    prueba2

--- Ejercicios/Ejercicio1/Program.cs
+++ Ejercicios/Ejercicio1/Program.cs
@@ -10,7 +10,17 @@ namespace Ejercicio1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hola");
+        
+            for (int i = 1; i <= 295; i++)
+            {
+                Console.WriteLine("{0}{1}{2}{3}", i, i % 3 == 0 || i % 5 == 0? " - ": string.Empty, i % 3 == 0 ? "Foo" : string.Empty,  i % 5 == 0 ? "Bar" : string.Empty);
+               
+            }
+            Console.Read();
         }
+        //git remote add origin https://github.com/PaulaLugo/CAI_Ejercicios.git
+        //git branch -M master
+        //git status
+
     }
 }
